06fe26c15dcf43e6cff5d469c99b1d24118c83b2
MaikolMolina <MolinaMaikol73@gmail.com>
2025-11-22 16:36:52 -0400

    ajuste wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Ajusta al path de tu settings.py
+# Ajusta según tu estructura
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_credi.settings')
+
+# Agregar el path del backend al sys.path
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
